d38e4355f30bf6aa4fbba50fac8c220f208e1d3a
viewBrzh <watayut.pa@vulcan.bluebik.com>
2025-02-24 00:26:03 +0700

    [FE] feat: add login page

---
+++ daily-task-update/src/pages/login/index.tsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import styles from '@/styles/Login.module.css';
+
+const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const router = useRouter();
+
+    const handleLogin = async () => {
+        if (!username || !password) {
+            setError("Please enter both username and password.");
+            return; // Stop further execution if validation fails
+        }
+
+        const response = await fetch("api/auth/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ username, password }),
+        });
+
+        const data = await response.json();
+        console.log(data);
+
+        if (response.ok) {
+            localStorage.setItem("token", data.token);
+            localStorage.setItem("fullName", data.user?.fullName);
+            router.push("/my-tasks");
+        } else {
+            setError("Invalid username or password");
+        }
+    };
+
+    return (
+        <>
+            <div className={styles.page}>
+                <div className={styles.triangle} />
+                <div className={styles.triangle} />
+                <div className={styles.triangle} />
+                <div className={styles.triangle} />
+                <div className={styles.triangle} />
+                <div className={styles.triangleBig} />
+                <div className={styles.triangleBig} />
+                <div className={styles.triangleBig} />
+                <div className={styles.triangleBig} />
+                <div className={styles.triangleBig} />
+                
+                <div className={styles.logo}><img src="/logo/logo-name.png" alt="Logo" /></div>
+
+                <div className={styles.container}>
+                    <div className={styles.promoTextContainer}>
+                        <div className={styles.promoText}>
+                            <h2><strong>Streamline your team's workflow</strong> with our intuitive project management platform—track tasks, manage sprints, and stay on top of deadlines with ease!</h2>
+                        </div>
+                    </div>
+                    <div className={styles.formContainer}>
+                        <h2 className={styles.title}>Login</h2>
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            className={styles.input}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className={styles.input}
+                        />
+                        <button onClick={handleLogin} className={styles.button}>Login</button>
+                        {error && <p className={styles.error}><small>{error}</small></p>}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Login;
