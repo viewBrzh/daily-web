de5c7d6511e3d2d2e4c954ddf207caee237de4c7
viewBrzh <watayut.pa@vulcan.bluebik.com>
2025-02-25 13:36:03 +0700

    [BE] delete duplicate code

--- back-end/api/index.js
+++ back-end/api/index.js
@@ -11,14 +11,10 @@ const dashboardRoute = require('../routes/dashboardRoute');
 const authenRoute = require('../routes/authenRoute');
 
 const app = express();
-const port = 10080;
-
-app.use(express.json());api\index.js
-app.use(bodyParser.json());
-
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json());  
+app.use(bodyParser.json())
 
 // Example route
 app.use("/hello", (req, res) => {
